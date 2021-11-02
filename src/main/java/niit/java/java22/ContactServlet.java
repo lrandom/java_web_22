f0bb4ad55b3df7d138ef8fa08b577d2a2fc915c3
Nguyen Thanh Luan <luann4099@gmail.com>
2021-11-02 21:31:59 +0700

    update code insert DB

--- src/main/java/niit/java/java22/ContactServlet.java
+++ src/main/java/niit/java/java22/ContactServlet.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class ContactServlet extends HttpServlet {
     @Override
@@ -14,5 +17,32 @@ public class ContactServlet extends HttpServlet {
         Writer writer = resp.getWriter();
         resp.setContentType("text/html");
         writer.write("<html><head></head><body>Test</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String fullName = req.getParameter("fullName");
+        String email = req.getParameter("email");
+        String note = req.getParameter("note");
+
+        Connection conn = null;
+        //kết nối vào CSDL để chèn vào bảng contact
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaweb_22", "root", "koodinh@");
+            PreparedStatement prp = conn.prepareStatement("INSERT INTO contacts(full_name,email,note) VALUES(?,?,?)");
+            prp.setString(1, fullName);
+            prp.setString(2, email);
+            prp.setString(3, note);
+            int count = prp.executeUpdate();
+            if (count > 0) {
+                System.out.println("Insert thành văn công");
+            }else{
+                System.out.println("Insert thất văn bại");
+            }
+        } catch (Exception e) {
+            System.out.println("Insert thất văn bại");
+            e.printStackTrace();
+        }
     }
 }
